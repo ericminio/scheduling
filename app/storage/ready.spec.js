a81ace3ff6789a6fe2f05ab9b4af3abab284ce55
ericminio <eric.mignot@gmail.com>
2021-07-11 14:31:27 -0700

    postgres config

--- app/storage/ready.spec.js
+++ app/storage/ready.spec.js
@@ -3,6 +3,17 @@ const { executeSync } = require('yop-postgresql');
 
 describe('database', ()=>{
 
+    it('discolses info', ()=>{
+        let connectionString = {
+            PGHOST: process.env.PGHOST,
+            PGDATABASE: process.env.PGDATABASE,
+            PGUSER: process.env.PGUSER,
+            PGPASSWORD: process.env.PGPASSWORD,
+            DATABASE_URL: process.env.DATABASE_URL
+        };
+        console.log('connection string', connectionString);        
+    });
+    
     it('can be reached', async ()=> {
         let rows = await executeSync('select 42 as number');
         expect(rows.length).to.equal(1);
