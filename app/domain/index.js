9672e214093dac3a94ef1d4c0e58f25983c4e336
Eric Mignot <eric.mignot@gmail.com>
2021-09-01 11:05:01 -0700

    new domain folder layout

--- app/domain/index.js
+++ app/domain/index.js
@@ -8,44 +8,44 @@ module.exports = {
     EventFactoryValidatingNeighbours: code('/domain/calendar/event-factory-validating-neighbours.js', 'EventFactoryValidatingNeighbours'),
     User: code('/domain/user/user.js', 'User'),
     Configuration: code('/domain/configuration/configuration.js', 'Configuration'),
-    isValidDate: code('/domain/calendar/is-valid-date.js', 'isValidDate'),
-    isValidDatetime: code('/domain/calendar/is-valid-datetime.js', 'isValidDatetime'),
+    isValidDate: code('/domain/rules/is-valid-date.js', 'isValidDate'),
+    isValidDatetime: code('/domain/rules/is-valid-datetime.js', 'isValidDatetime'),
     nextDay: code('/domain/calendar/time.js', 'nextDay'),
     previousDay: code('/domain/calendar/time.js', 'previousDay'),
-    isAnOverbooking: code('/domain/calendar/is-overbooking.js', 'isAnOverbooking'),
+    isAnOverbooking: code('/domain/rules/is-overbooking.js', 'isAnOverbooking'),
     formatDate: code('/domain/calendar/time.js', 'formatDate'),
 
-    SearchEvents: code('/domain/calendar/search-events.js', 'SearchEvents'),
-    CreateEvent: code('/domain/calendar/create-event.js', 'CreateEvent'),
+    SearchEvents: code('/domain/services/search-events.js', 'SearchEvents'),
+    CreateEvent: code('/domain/services/create-event.js', 'CreateEvent'),
     CreateEventWithDependencies: codes([
-        '/domain/calendar/is-valid-label.js',
-        '/domain/calendar/is-valid-datetime.js',
-        '/domain/calendar/is-overbooking.js',
+        '/domain/rules/is-valid-label.js',
+        '/domain/rules/is-valid-datetime.js',
+        '/domain/rules/is-overbooking.js',
         '/domain/calendar/event.js',
         '/domain/calendar/resource.js',
         '/domain/calendar/event-factory-validating-fields.js',
         '/domain/calendar/event-factory-validating-neighbours.js',
-        '/domain/calendar/create-event.js'
+        '/domain/services/create-event.js'
     ], 'CreateEvent'),
-    DeleteEvent: code('/domain/calendar/delete-event.js', 'DeleteEvent'),
+    DeleteEvent: code('/domain/services/delete-event.js', 'DeleteEvent'),
 
-    CreateResource: code('/domain/calendar/create-resource.js', 'CreateResource'),
+    CreateResource: code('/domain/services/create-resource.js', 'CreateResource'),
     CreateResourceWithDependencies: codes([
         '/domain/calendar/resource.js',
         '/domain/calendar/resource-factory.js',
-        '/domain/calendar/create-resource.js'
+        '/domain/services/create-resource.js'
     ], 'CreateResource'),
 
     EventFactoryValidatingFieldsWithDependencies: codes([
-        '/domain/calendar/is-valid-label.js',
-        '/domain/calendar/is-valid-datetime.js',
+        '/domain/rules/is-valid-label.js',
+        '/domain/rules/is-valid-datetime.js',
         '/domain/calendar/event.js',
         '/domain/calendar/event-factory-validating-fields.js'
     ], 'EventFactoryValidatingFields'),
     EventFactoryValidatingNeighboursWithDependencies: codes([
-        '/domain/calendar/is-valid-label.js',
-        '/domain/calendar/is-valid-datetime.js',
-        '/domain/calendar/is-overbooking.js',
+        '/domain/rules/is-valid-label.js',
+        '/domain/rules/is-valid-datetime.js',
+        '/domain/rules/is-overbooking.js',
         '/domain/calendar/event.js',
         '/domain/calendar/resource.js',
         '/domain/calendar/event-factory-validating-fields.js',
@@ -55,24 +55,24 @@ module.exports = {
         '/domain/calendar/resource.js',
         '/domain/calendar/resource-factory.js'
     ], 'ResourceFactory'),
-    DeleteResource: code('/domain/calendar/delete-resource.js', 'DeleteResource'),
-    GetResources: code('/domain/calendar/get-resources.js', 'GetResources'),
+    DeleteResource: code('/domain/services/delete-resource.js', 'DeleteResource'),
+    GetResources: code('/domain/services/get-resources.js', 'GetResources'),
 
     domain: contents([
         '/domain/configuration/configuration.js',
         '/domain/calendar/resource.js',
         '/domain/calendar/event.js',
         '/domain/calendar/time.js',
-        '/domain/calendar/is-valid-date.js',
-        '/domain/calendar/is-valid-datetime.js',
-        '/domain/calendar/is-valid-label.js',
+        '/domain/rules/is-valid-date.js',
+        '/domain/rules/is-valid-datetime.js',
+        '/domain/rules/is-valid-label.js',
         '/domain/calendar/event-factory-validating-fields.js',
-        '/domain/calendar/create-event.js',
-        '/domain/calendar/delete-event.js',
-        '/domain/calendar/search-events.js',
+        '/domain/services/create-event.js',
+        '/domain/services/delete-event.js',
+        '/domain/services/search-events.js',
         '/domain/calendar/resource-factory.js',
-        '/domain/calendar/create-resource.js',
-        '/domain/calendar/delete-resource.js',
-        '/domain/calendar/get-resources.js'
+        '/domain/services/create-resource.js',
+        '/domain/services/delete-resource.js',
+        '/domain/services/get-resources.js'
     ])
 }
